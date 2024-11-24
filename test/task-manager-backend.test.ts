005a0dbb4ec46d137957a219201001e8a638d338
nuferkei <stephankei.nufer@raiffeisen.ch>
2024-11-24 10:49:34 +0100

    Initial commit

---
+++ test/task-manager-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TaskManagerBackend from '../lib/task-manager-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/task-manager-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TaskManagerBackend.TaskManagerBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
