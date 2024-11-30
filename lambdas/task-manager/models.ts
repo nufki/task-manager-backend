50b8f0b04869b3b2dbecd2f5fec462cf30684a4d
nuferkei <stephankei.nufer@raiffeisen.ch>
2024-11-30 18:14:16 +0100

    changed entire task-manager lambda to typescript and changed to NodejsFunction as this is better managed with ESBuilds

---
+++ lambdas/task-manager/models.ts
@@ -0,0 +1,21 @@
+export enum TaskPriority {
+    Low = "low",
+    Medium = "medium",
+    High = "high",
+}
+
+export enum TaskStatus {
+    NotStarted = "not-started",
+    InProgress = "in-progress",
+    Completed = "completed",
+}
+
+export interface Task {
+    id: string; // Generated UIIDV4
+    userId: string;  // The ID of the user who owns / created this task
+    name: string;
+    description: string;
+    status: TaskStatus;
+    priority: TaskPriority;
+    dueDate: Date; 
+}
