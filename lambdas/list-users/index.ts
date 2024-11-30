264ef0a3542b9837982d8b1ecff032c94f7fa29a
nuferkei <stephankei.nufer@raiffeisen.ch>
2024-11-30 19:28:58 +0100

    initial commit

---
+++ lambdas/list-users/index.ts
@@ -0,0 +1,45 @@
+import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { CognitoIdentityProviderClient, ListUsersCommand } from "@aws-sdk/client-cognito-identity-provider";
+
+const cognitoClient = new CognitoIdentityProviderClient({});
+const userPoolId = process.env.USER_POOL_ID!;
+
+const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+};
+
+export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        const queryParams = event.queryStringParameters || {};
+        const searchUsername = queryParams.username;
+        const nextToken = queryParams.nextToken;
+        const limit = queryParams.limit ? parseInt(queryParams.limit, 10) : 10;
+
+        const command = new ListUsersCommand({
+            UserPoolId: userPoolId,
+            Limit: limit,
+            ...(nextToken && { PaginationToken: nextToken }),
+            ...(searchUsername && { Filter: `username ^= "${searchUsername}"` }),
+        });
+
+        var response = await cognitoClient.send(command);
+
+
+        return {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify({
+                users: response.Users,
+                nextToken: response.PaginationToken,
+            }),
+        };
+    } catch (error) {
+        console.error('Error listing users:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to list users' }),
+        };
+    }
+};
--- lib/task-manager-backend-stack.ts
+++ lib/task-manager-backend-stack.ts
@@ -1,100 +1,160 @@
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
-import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Construct} from 'constructs';
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class TaskManagerBackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // DynamoDB Table
-    const taskTable = new dynamodb.Table(this, 'TaskTable', {
-      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-    });
+        // DynamoDB Table
+        const taskTable = new dynamodb.Table(this, 'TaskTable', {
+            partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING},
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        });
 
-    // Create Task Manager Lambda
-    const taskFunction = new NodejsFunction(this, 'TaskFunction', {
-      entry: 'lambdas/task-manager/index.ts', // Path to your Lambda function code
-      handler: 'handler',
-      runtime: lambda.Runtime.NODEJS_18_X,
-      environment: {
-        TABLE_NAME: taskTable.tableName,
-      },
-    });
 
-    taskTable.grantReadWriteData(taskFunction);
+        // Cognito User Pool
+        const userPool = new cognito.UserPool(this, 'TaskManagerUserPool', {
+            selfSignUpEnabled: true,
+            userVerification: {
+                emailSubject: 'Verify your email in my app!',
+                emailBody: 'Thanks for signing up! Your verification code is {####}',
+                emailStyle: cognito.VerificationEmailStyle.CODE,
+            },
+            signInAliases: {
+                email: true,
+                username: true
+            },
+        });
 
-    // Cognito User Pool
-    const userPool = new cognito.UserPool(this, 'TaskManagerUserPool', {
-      selfSignUpEnabled: true,
-      userVerification: {
-        emailSubject: 'Verify your email in my app!',
-        emailBody: 'Thanks for signing up! Your verification code is {####}',
-        emailStyle: cognito.VerificationEmailStyle.CODE,
-      },
-      signInAliases: {
-        email: true,
-        username: true
-      },
-    });
+        const userPoolClient = new cognito.UserPoolClient(this, 'TaskManagerUserPoolClient', {
+            userPool,
+            generateSecret: false,
+        });
 
-    const userPoolClient = new cognito.UserPoolClient(this, 'TaskManagerUserPoolClient', {
-      userPool,
-      generateSecret: false,
-    });
+        // Output the User Pool ID and Client ID
+        new cdk.CfnOutput(this, 'UserPoolId', {
+            value: userPool.userPoolId,
+            description: 'The ID of the Cognito User Pool',
+        });
 
-    // Output the User Pool Client ID
-    new cdk.CfnOutput(this, 'UserPoolClientId', {
-      value: userPoolClient.userPoolClientId,
-      description: 'Client ID for Cognito User Pool (for frontend integration)',
-    });
+        // Output the User Pool Client ID
+        new cdk.CfnOutput(this, 'UserPoolClientId', {
+            value: userPoolClient.userPoolClientId,
+            description: 'Client ID for Cognito User Pool (for frontend integration)',
+        });
 
-    // API Gateway
-    const api = new apigateway.RestApi(this, 'TaskApi', {
-      restApiName: 'Task Service',
-      defaultCorsPreflightOptions: {
-        allowOrigins: apigateway.Cors.ALL_ORIGINS,
-        allowMethods: apigateway.Cors.ALL_METHODS,
-        allowHeaders: ['Content-Type', 'X-Amz-Date', 'Authorization', 'X-Api-Key', 'X-Amz-Security-Token'],
-      },
-    });
 
-    const authorizer = new apigateway.CognitoUserPoolsAuthorizer(this, 'TaskApiAuthorizer', {
-      cognitoUserPools: [userPool],
-    });
+        /******************************************************
+        /* Add Task API
+        /******************************************************/
+        
+        // Create Task Manager lambda function
+        const taskFunction = new NodejsFunction(this, 'TaskFunction', {
+                entry: 'lambdas/task-manager/index.ts',
+                handler: 'handler',
+                runtime: lambda.Runtime.NODEJS_18_X,
+                environment: {
+                    TABLE_NAME: taskTable.tableName,
+                },
+            });
+        taskTable.grantReadWriteData(taskFunction);
 
-    const tasks = api.root.addResource('tasks');
-    const singleTask = tasks.addResource('{id}');
+        // API Gateway
+        const taskAPI = new apigateway.RestApi(this, 'TaskApi', {
+            restApiName: 'Task Service',
+            defaultCorsPreflightOptions: {
+                allowOrigins: apigateway.Cors.ALL_ORIGINS,
+                allowMethods: apigateway.Cors.ALL_METHODS,
+                allowHeaders: ['Content-Type', 'X-Amz-Date', 'Authorization', 'X-Api-Key', 'X-Amz-Security-Token'],
+            },
+        });
 
-    // Protect endpoints with Cognito Authorizer
-    tasks.addMethod('POST', new apigateway.LambdaIntegration(taskFunction), {
-      authorizer,
-      authorizationType: apigateway.AuthorizationType.COGNITO,
-    });
+        const taskAuthorizer = new apigateway.CognitoUserPoolsAuthorizer(this, 'TaskApiAuthorizer', {
+            cognitoUserPools: [userPool],
+        });
 
-    tasks.addMethod('GET', new apigateway.LambdaIntegration(taskFunction), {
-      authorizer,
-      authorizationType: apigateway.AuthorizationType.COGNITO,
-    });
+        // Define the /tasks resource
+        const tasks = taskAPI.root.addResource('tasks');
+        const singleTask = tasks.addResource('{id}');
 
-    singleTask.addMethod('GET', new apigateway.LambdaIntegration(taskFunction), {
-      authorizer,
-      authorizationType: apigateway.AuthorizationType.COGNITO,
-    });
+        // Protect endpoints with Cognito Authorizer
+        tasks.addMethod('POST', new apigateway.LambdaIntegration(taskFunction), {
+            authorizer: taskAuthorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
+        });
 
-    singleTask.addMethod('PUT', new apigateway.LambdaIntegration(taskFunction), {
-      authorizer,
-      authorizationType: apigateway.AuthorizationType.COGNITO,
-    });
+        tasks.addMethod('GET', new apigateway.LambdaIntegration(taskFunction), {
+            authorizer: taskAuthorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
+        });
 
-    singleTask.addMethod('DELETE', new apigateway.LambdaIntegration(taskFunction), {
-      authorizer,
-      authorizationType: apigateway.AuthorizationType.COGNITO,
-    });
-  }
+        singleTask.addMethod('GET', new apigateway.LambdaIntegration(taskFunction), {
+            authorizer: taskAuthorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
+        });
+
+        singleTask.addMethod('PUT', new apigateway.LambdaIntegration(taskFunction), {
+            authorizer: taskAuthorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
+        });
+
+        singleTask.addMethod('DELETE', new apigateway.LambdaIntegration(taskFunction), {
+            authorizer: taskAuthorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
+        });
+
+
+
+       /******************************************************
+       /* dd ListUsers API resources:
+       /******************************************************/
+
+        // Create List user lambda function
+        const listUserFunction = new NodejsFunction(this, 'ListUsersLambda', {
+                entry: 'lambdas/list-users/index.ts',
+                handler: 'handler',
+                runtime: lambda.Runtime.NODEJS_18_X,
+                environment: {
+                    USER_POOL_ID: userPool.userPoolId,
+                },
+            });
+
+        // Grant permissions to the Lambda function
+        listUserFunction.addToRolePolicy(
+            new iam.PolicyStatement({
+                actions: ['cognito-idp:ListUsers'],
+                resources: ['*'], // Consider scoping to your User Pool ARN
+            })
+        );
+
+        // Define API Gateway
+        const userAPI = new apigateway.RestApi(this, 'ListUsersApi', {
+            restApiName: 'User Service',
+            defaultCorsPreflightOptions: {
+                allowOrigins: apigateway.Cors.ALL_ORIGINS,
+                allowMethods: apigateway.Cors.ALL_METHODS,
+                allowHeaders: ['Content-Type', 'X-Amz-Date', 'Authorization', 'X-Api-Key', 'X-Amz-Security-Token'],
+            },
+        });
+
+
+        const userAuthorizer = new apigateway.CognitoUserPoolsAuthorizer(this, 'UserApiAuthorizer', {
+            cognitoUserPools: [userPool],
+        });
+
+        // Define the /users resource
+        const users = userAPI.root.addResource('users');
+
+        // List User APIs
+        users.addMethod('GET', new apigateway.LambdaIntegration(listUserFunction), {
+            authorizer: userAuthorizer,
+            authorizationType: apigateway.AuthorizationType.COGNITO,
+        });
+    }
 }
